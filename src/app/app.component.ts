6b058e50b0d4b5b087d8192cbafc3af09d04bb85
osmanBAyn <osbaran10@gmail.com>
2024-02-05 12:43:08 +0300

    Init and base components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, InputSignal, OnInit, Signal, WritableSignal, computed, effect, input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { setInterval } from 'timers/promises';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'play1';
+export class AppComponent{
+  items = [1,1,1,1];
+
 }
