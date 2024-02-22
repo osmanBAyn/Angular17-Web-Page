eb7a8a130c11f32235c88b82ce48b87480389155
osmanBAyn <osbaran10@gmail.com>
2024-02-22 12:51:08 +0300

    Created Perfection component

---
+++ src/app/perfection/perfection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfection',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfection.component.html',
+  styleUrl: './perfection.component.css'
+})
+export class PerfectionComponent {
+
+}
